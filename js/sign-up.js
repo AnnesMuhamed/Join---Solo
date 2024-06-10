6764156b7baa7c8392ccda3a4c094e0fb9806689
ApfelNektar <149399980+AnnesMuhamed@users.noreply.github.com>
2024-06-10 19:08:22 +0200

    Added Firbase URL

--- js/sign-up.js
+++ js/sign-up.js
@@ -1,3 +1,37 @@
+const BASE_URL = "https://join-232-default-rtdb.europe-west1.firebasedatabase.app/";
+
+function onloadFunc() {
+    console.log("test");
+    loadData();
+}
+
+async function loadData(path= "") {
+    let response = await fetch(BASE_URL + path + ".json");
+    return responseToJson = await response.json();
+    
+}
+ 
+async function postData(path="", data={}) {
+    let response = await fetch(BASE_URL + path + ".json",{
+        method: "POST",
+        header: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data)
+    });
+return responseToJson = await response.json();
+ 
+}
+ 
+async function deleteData(path="") {
+let response = await fetch(BASE_URL + path + ".json",{
+    method: "DELETE",
+});
+return responseToJson = await response.json();
+ 
+}
+
+
 function init() {
     let signUp = document.getElementById('sectionSignUp');
 
