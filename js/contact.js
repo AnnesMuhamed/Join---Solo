f367be25a9bb5913f2b1b485e0ecb3d35bf81120
ApfelNektar <149399980+AnnesMuhamed@users.noreply.github.com>
2024-06-12 16:40:14 +0200

    Create Contact Page

--- js/contact.js
+++ js/contact.js
@@ -1,34 +1,58 @@
 let contacts = [
-    { firstName: 'Anton', lastName: 'Adler', email: 'anton.adler@example.com', phone: '123-456-7890' },
-    { firstName: 'Berta', lastName: 'Baumann', email: 'berta.baumann@example.com', phone: '123-456-7891' },
-    { firstName: 'Cäsar', lastName: 'Cramer', email: 'caesar.cramer@example.com', phone: '123-456-7892' },
-    { firstName: 'Dora', lastName: 'Dürr', email: 'dora.duerr@example.com', phone: '123-456-7893' },
-    { firstName: 'Emil', lastName: 'Eckert', email: 'emil.eckert@example.com', phone: '123-456-7894' },
-    { firstName: 'Friedrich', lastName: 'Fischer', email: 'friedrich.fischer@example.com', phone: '123-456-7895' },
-    { firstName: 'Gustav', lastName: 'Gärtner', email: 'gustav.gaertner@example.com', phone: '123-456-7896' },
-    { firstName: 'Heinrich', lastName: 'Hoffmann', email: 'heinrich.hoffmann@example.com', phone: '123-456-7897' },
-    { firstName: 'Ida', lastName: 'Illner', email: 'ida.illner@example.com', phone: '123-456-7898' },
-    { firstName: 'Julius', lastName: 'Jäger', email: 'julius.jaeger@example.com', phone: '123-456-7899' }
+    { firstName: 'Anton', lastName: 'Adler', email: 'anton@gmc.com', initials: 'AA' },
+    { firstName: 'Berta', lastName: 'Baumann', email: 'berta@gmc.com', initials: 'BB' },
+    { firstName: 'Cäsar', lastName: 'Cramer', email: 'caesar@gmc.com', initials: 'CC' },
+    { firstName: 'Dora', lastName: 'Dürr', email: 'dora@gmc.com', initials: 'DD' },
+    { firstName: 'Emil', lastName: 'Eckert', email: 'emil@gmc.com', initials: 'EE' },
+    { firstName: 'Friedrich', lastName: 'Fischer', email: 'friedrich@gmc.com', initials: 'FF' },
+    { firstName: 'Gustav', lastName: 'Gärtner', email: 'gustav@gmc.com', initials: 'GG' },
+    { firstName: 'Heinrich', lastName: 'Hoffmann', email: 'heinrich@gmc.com', initials: 'HH' },
+    { firstName: 'Ida', lastName: 'Illner', email: 'ida@gmc.com', initials: 'II' },
+    { firstName: 'Julius', lastName: 'Jäger', email: 'julius@gmc.com', initials: 'JJ' }
 ];
+let alphabetSections = [];
 
 function renderContacts() {
     let contactSection = document.getElementById('contactSection');
-    
-    contactSection.innerHTML = `
-        <button onclick="addNewContact()">Add a new Contact</button>
-        <div class="contact-list-container">
-            ${contacts.map(contact => `
-                <div class="contact-item">
-                    <div>${contact.firstName} ${contact.lastName}</div>
-                    <div>${contact.email}</div>
-                    <div>${contact.phone}</div>
+    let currentLetter = '';
+
+    contacts.forEach(contact => {
+        let firstLetter = contact.lastName.charAt(0).toUpperCase();
+        if (firstLetter !== currentLetter) {
+            currentLetter = firstLetter;
+            alphabetSections.push(`
+                <div class="alphabet-container">
+                    <span>${currentLetter}</span>
                 </div>
-            `).join('')}
+            `);
+        }
+        alphabetSections.push(`
+            <div class="separator"></div>
+            <div class="contact-item">
+                <div class="initials-container">${contact.initials}</div>
+                <div class="contact-info-item">
+                    <div class="contact-info">
+                        <div class="contact-names">${contact.firstName} ${contact.lastName}</div>
+                        <div class="contact-email">${contact.email}</div>
+                    </div>
+                </div>
+                
+            </div>
+        `);
+    });
+
+    contactSection.innerHTML = `
+        <button onclick="addNewContact()" class="add-contact-button">
+            <span class="button-text">Add a new contact</span>
+            <img src="/img/person_add.png" alt="Add Icon" class="button-icon">
+        </button>
+        <div class="contact-list-container">
+            ${alphabetSections.join('')}
         </div>
+
     `;
 }
 
 function addNewContact() {
-    // Logik zum Hinzufügen eines neuen Kontakts
     alert("Neuen Kontakt hinzufügen");
 }
