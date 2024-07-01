37637904e9a14f30cc6b9c6a94dd6506b8bb5393
RiGaHo <github.zxdmf@slmails.com>
2024-07-01 17:21:30 +0200

    Added contacts functionality in dropdown test

--- add-task/dropdown-test/script.js
+++ add-task/dropdown-test/script.js
@@ -1,4 +1,51 @@
+const BASE_URL = 'https://join-232-default-rtdb.europe-west1.firebasedatabase.app/';
+let path = 'contacts';
 let expanded = false;
+let contacts = null;
+
+
+async function initFunc() {
+	await loadContacts();
+	renderCheckboxes();
+}
+
+
+async function loadData(path='') {
+	let response = await fetch(`${BASE_URL}${path}.json`);
+	let responseToJson = await response.json();
+	return responseToJson;
+}
+
+
+async function loadContacts() {
+	contacts = await loadData(path);
+}
+
+
+function getContactsIds() {
+	let contactsIds = [];
+	for(let id in contacts) {
+		contactsIds.push(id);
+	}
+	return contactsIds;
+}
+
+
+function renderCheckboxes() {
+	let contactsIds = getContactsIds();
+	let checkboxes = document.getElementById('checkboxes');
+	checkboxes.innerHTML = '';
+	for(let n = 0; n < contactsIds.length; n++) {
+		let id = contactsIds[n];
+		checkboxes.innerHTML += `
+			<label for="${id}">
+				<span>${contacts[id]['firstName']} ${contacts[id]['lastName']}</span>
+				<input type="checkbox" id="${id}">
+			</label>
+		`;
+	}
+}
+
 
 function showCheckboxes() {
 	let checkboxes = document.getElementById('checkboxes');
