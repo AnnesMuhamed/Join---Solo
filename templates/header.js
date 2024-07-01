b1310f3fdecf862791d9c83ec20fff0182e032f4
Cenk Korkmaz <Cenk.Korkmaz92@gmail.com>
2024-07-01 15:03:51 +0200

    added dropdown menu for user - linked in all file sbefore end of body

--- templates/header.js
+++ templates/header.js
@@ -23,4 +23,21 @@ async function displayUserInitials(userID) {
     }
 }
 
+function toggleDropdown() {
+    document.getElementById("user-dropdown").classList.toggle("show");
+}
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function (event) {
+    if (!event.target.matches('#user')) {
+        var dropdowns = document.getElementsByClassName("dropdown-content");
+        for (var i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show')) {
+                openDropdown.classList.remove('show');
+            }
+        }
+    }
+}
+
 displayUserInitials('user123');
