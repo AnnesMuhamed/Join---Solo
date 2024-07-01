676e235c17d62ebdf66671ca1a02efa52e308bef
RiGaHo <github.zxdmf@slmails.com>
2024-07-02 00:18:40 +0200

    Dropdown functionality filter and collapse through click in body added

--- add-task/dropdown-test/script.js
+++ add-task/dropdown-test/script.js
@@ -4,6 +4,12 @@ let expanded = false;
 let contacts = null;
 
 
+document.addEventListener('DOMContentLoaded', function() {
+	initFunc();
+	document.body.addEventListener('click', collapseCheckboxes);
+});
+
+
 async function initFunc() {
 	await loadContacts();
 	renderCheckboxes();
@@ -31,14 +37,32 @@ function getContactsIds() {
 }
 
 
+function getInitials(id) {
+	return `${contacts[id]['firstName'].charAt(0)}${contacts[id]['lastName'].charAt(0)}`;
+}
+
+
+function filterCheckboxes(id) {
+	let filter = document.getElementById('search');
+	let inputValue = filter.value.toLowerCase();
+	let fullName = `${contacts[id].firstName.toLowerCase()} ${contacts[id].lastName.toLowerCase()}`;
+	return (fullName.includes(inputValue));
+}
+
+
 function renderCheckboxes() {
 	let contactsIds = getContactsIds();
 	let checkboxes = document.getElementById('checkboxes');
 	checkboxes.innerHTML = '';
 	for(let n = 0; n < contactsIds.length; n++) {
 		let id = contactsIds[n];
+		let initials = getInitials(id);
+		if(!(filterCheckboxes(id))) {
+			continue;
+		}
 		checkboxes.innerHTML += `
 			<label for="${id}">
+				<span>${initials}</span>
 				<span>${contacts[id]['firstName']} ${contacts[id]['lastName']}</span>
 				<input type="checkbox" id="${id}">
 			</label>
@@ -53,6 +77,18 @@ function showCheckboxes() {
 		checkboxes.style.display = 'block';
 		expanded = true;
 	} else {
+		checkboxes.style.display = 'none';
+		expanded = false;
+	}
+}
+
+
+function collapseCheckboxes(event) {
+	let checkboxes = document.getElementById('checkboxes');
+	let search = document.getElementById('search');
+	if(!event.target.closest('.wrapper')) {
+		search.value = '';
+		renderCheckboxes();
 		checkboxes.style.display = 'none';
 		expanded = false;
 	}
