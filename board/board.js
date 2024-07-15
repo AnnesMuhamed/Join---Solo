11fedbd1cbc7e4077038eba258a25e59b22a130f
ApfelNektar <149399980+AnnesMuhamed@users.noreply.github.com>
2024-07-15 17:23:28 +0200

    add New Task Form

--- board/board.js
+++ board/board.js
@@ -1,77 +1,104 @@
 'use strict';
+
 async function includeHTML() {
-  let includeElements = document.querySelectorAll("[w3-include-html]");
-  for (let i = 0; i < includeElements.length; i++) {
-    const element = includeElements[i];
-    let file = element.getAttribute("w3-include-html"); // "includes/header.html"
-    let resp = await fetch(file);
-    if (resp.ok) {
-      element.innerHTML = await resp.text();
-    } else {
-      element.innerHTML = "Page not found";
+    let includeElements = document.querySelectorAll("[w3-include-html]");
+    for (let i = 0; i < includeElements.length; i++) {
+        const element = includeElements[i];
+        let file = element.getAttribute("w3-include-html"); // "includes/header.html"
+        let resp = await fetch(file);
+        if (resp.ok) {
+            element.innerHTML = await resp.text();
+        } else {
+            element.innerHTML = "Page not found";
+        }
     }
-  }
+}
+
+async function loadData(path = "") {
+    let response = await fetch(BASE_URL + path + ".json");
+    return await response.json();
 }
 
 let todos = [{
-  'id': 0,
-  'title': 'User Story',
-  'category': 'open'
+    'id': 0,
+    'title': 'User Story',
+    'category': 'open'
 }, {
-  'id': 1,
-  'title': 'Technical Task',
-  'category': 'open'
+    'id': 1,
+    'title': 'Technical Task',
+    'category': 'open'
 }, {
-  'id': 2,
-  'title': 'Technical Task',
-  'category': 'closed'
+    'id': 2,
+    'title': 'Technical Task',
+    'category': 'closed'
 }];
 
 let currentDraggedElement;
 
 function updateHTML() {
-  let open = todos.filter(t => t['category'] == 'open');
+    let open = todos.filter(t => t['category'] == 'open');
 
-  document.getElementById('open').innerHTML = '';
+    document.getElementById('open').innerHTML = '';
 
-  for (let index = 0; index < open.length; index++) {
-      const element = open[index];
-      document.getElementById('open').innerHTML += generateTodoHTML(element);
-  }
+    for (let index = 0; index < open.length; index++) {
+        const element = open[index];
+        document.getElementById('open').innerHTML += generateTodoHTML(element);
+    }
 
-  let closed = todos.filter(t => t['category'] == 'closed');
+    let closed = todos.filter(t => t['category'] == 'closed');
 
-  document.getElementById('closed').innerHTML = '';
+    document.getElementById('closed').innerHTML = '';
 
-  for (let index = 0; index < closed.length; index++) {
-      const element = closed[index];
-      document.getElementById('closed').innerHTML += generateTodoHTML(element);
-  }
-  includeHTML();
+    for (let index = 0; index < closed.length; index++) {
+        const element = closed[index];
+        document.getElementById('closed').innerHTML += generateTodoHTML(element);
+    }
+    includeHTML();
 }
 
 function startDragging(id) {
-  currentDraggedElement = id;
+    currentDraggedElement = id;
 }
 
 function generateTodoHTML(element) {
-  return `<div draggable="true" ondragstart="startDragging(${element['id']})" class="todo">${element['title']}</div>`;
+    return `<div draggable="true" ondragstart="startDragging(${element['id']})" class="todo">${element['title']}</div>`;
 }
 
 function allowDrop(ev) {
-  ev.preventDefault();
+    ev.preventDefault();
 }
 
 function moveTo(category) {
-  todos[currentDraggedElement]['category'] = category;
-  updateHTML();
+    todos[currentDraggedElement]['category'] = category;
+    updateHTML();
 }
 
 function highlight(id) {
-  document.getElementById(id).classList.add('drag-area-highlight');
+    document.getElementById(id).classList.add('drag-area-highlight');
 }
 
 function removeHighlight(id) {
-  document.getElementById(id).classList.remove('drag-area-highlight');
+    document.getElementById(id).classList.remove('drag-area-highlight');
 }
 
+function openAddTaskForm() {
+    openForm('newTask');
+}
+
+function openForm(formId) {
+    document.getElementById(formId).classList.add('show');
+    document.getElementById('overlay').style.display = 'block';
+    document.body.classList.add('modal-open');
+}
+
+function closeAddTaskForm() {
+    closeForm('newTask');
+}
+
+function closeForm(formId) {
+    document.getElementById(formId).classList.remove('show');
+    document.getElementById('overlay').style.display = 'none';
+    document.body.classList.remove('modal-open');
+}
+
+document.addEventListener("DOMContentLoaded", updateHTML);
