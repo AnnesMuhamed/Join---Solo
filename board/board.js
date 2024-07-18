7520769fe9c35dbd93517fc18ac4a6c02753d3e7
ApfelNektar <149399980+AnnesMuhamed@users.noreply.github.com>
2024-07-18 22:37:01 +0200

    Corrected Drag/Drop Container

--- board/board.js
+++ board/board.js
@@ -20,68 +20,49 @@ async function loadData(path = "") {
 
 let todos = [{
   'id': 0,
-  'title': 'User Story',
+  'title': 'Putzen',
   'category': 'open'
 }, {
   'id': 1,
-  'title': 'Technical Task',
-  'category': 'open'
-}, {
-  'id': 2,
-  'title': 'Technical Task',
+  'title': 'Kochen',
   'category': 'closed'
 }];
 
 let currentDraggedElement;
 
 function updateHTML() {
-  const categories = ['to-do', 'in-progress', 'await-feedback', 'done'];
-
+  const categories = ['open', 'closed', 'in-progress', 'done'];
+  
   categories.forEach(category => {
-    let tasks = todos.filter(t => t['category'] === category);
-    document.getElementById(category).innerHTML = '';
-
-    for (let index = 0; index < tasks.length; index++) {
-      const element = tasks[index];
-      document.getElementById(category).innerHTML += generateTodoHTML(element);
-    }
+      let elements = todos.filter(t => t['category'] == category);
+      document.getElementById(category).innerHTML = '';
+      for (let index = 0; index < elements.length; index++) {
+          const element = elements[index];
+          document.getElementById(category).innerHTML += generateTodoHTML(element);
+      }
   });
-
   includeHTML();
+}
+
+function startDragging(id) {
+  currentDraggedElement = id;
+}
+
+function generateTodoHTML(element) {
+  return `
+    <div onclick="openUserStory('${element.id}')" class="todo-card" draggable="true" ondragstart="startDragging(${element['id']})" ${element['title']}">
+        <h3 class="tasks-cards-headline">${element.title}</h3>
+    </div>
+    `;   
 }
 
 function allowDrop(ev) {
   ev.preventDefault();
 }
 
-function highlight(category) {
-  document.getElementById(category).classList.add('highlight');
-}
-
-function removeHighlight(category) {
-  document.getElementById(category).classList.remove('highlight');
-}
-
-function moveTo(ev, category) {
-  ev.preventDefault();
-  const todoId = ev.dataTransfer.getData("text");
-  const todoIndex = todos.findIndex(t => t.id == todoId);
-  if (todoIndex > -1) {
-    todos[todoIndex].category = category;
-    updateHTML();
-  }
-}
-
-function generateTodoHTML(todo) {
-  return `
-    <div onclick="openUserStory('${todo.id}')" class="todo-card" draggable="true" ondragstart="drag(event)" id="${todo.id}">
-        <h3 class="tasks-cards-headline">${todo.title}</h3>
-    </div>
-    `;   
-}
-
-function drag(ev) {
-  ev.dataTransfer.setData("text", ev.target.id);
+function moveTo(category) {
+  todos[currentDraggedElement]['category'] = category;
+  updateHTML();
 }
 
 function openAddTaskForm() {
