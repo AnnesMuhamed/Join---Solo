ff23cab439393b59311ff51550054ed5d19314d6
ApfelNektar <149399980+AnnesMuhamed@users.noreply.github.com>
2024-12-02 14:10:33 +0100

    Privacy Policy and Legal Notice Corrected

--- js/legal-notice.js
+++ js/legal-notice.js
@@ -1,15 +1,20 @@
 'use strict';
 
 async function includeHTML() {
-  let includeElements = document.queryselectorall("[w3-include-html]");
+  let includeElements = document.querySelectorAll("[w3-include-html]");
   for (let i = 0; i < includeElements.length; i++) {
     const element = includeElements[i];
-    let file = element.getattribute("w3-include-html"); // "includes/header.html"
-    let resp = await fetch(file);
-    if (resp.ok) {
-      element.innerhtml = await resp.text();
-    } else {
-      element.innerhtml = "Page not found";
+    let file = element.getAttribute("w3-include-html"); 
+    try {
+      let resp = await fetch(file);
+      if (resp.ok) {
+        element.innerHTML = await resp.text();
+      } else {
+        element.innerHTML = "Page not found";
+      }
+    } catch (error) {
+      console.error(`Error fetching the file "${file}":`, error);
+      element.innerHTML = "Error loading content";
     }
   }
 }
